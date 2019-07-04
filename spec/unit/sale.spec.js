b017b413746f17eeaeb82029f799854b1ab89375
shektor <s.hektor@gmail.com>
2019-07-04 16:51:59 +0100

    Create total function for Sale class
    Will lookup each product in the order against the price list and
    multiply by quantity, adding to a total that is returned.

--- spec/unit/sale.spec.js
+++ spec/unit/sale.spec.js
@@ -1,4 +1,5 @@
 const { Sale } = require('../../src/models/sale')
+const pricing = require('../../data/pricing')
 
 describe('Sale', () => {
   describe('::new', () => {
@@ -6,6 +7,27 @@ describe('Sale', () => {
       const sale = new Sale()
 
       expect(sale).toBeInstanceOf(Sale)
+    })
+  })
+
+  describe('#total', () => {
+    it('returns order total for items with no VAT', () => {
+      const order = {
+        items: [
+          {
+            product_id: 2,
+            quantity: 1
+          },
+          {
+            product_id: 3,
+            quantity: 2
+          }
+        ]
+      }
+
+      const sale = new Sale(order, pricing)
+
+      expect(sale.total()).toBe(750)
     })
   })
 })
--- src/models/sale.js
+++ src/models/sale.js
@@ -1,5 +1,18 @@
 class Sale {
-  constructor () {}
+  constructor (order, prices) {
+    this.order = order
+    this.prices = prices
+  }
+
+  total () {
+    let total = 0
+
+    this.order.items.forEach((item) => {
+      total += this.prices.product[item.product_id].price * item.quantity
+    })
+
+    return total
+  }
 }
 
 module.exports = {
