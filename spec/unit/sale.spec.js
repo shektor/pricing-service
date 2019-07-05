ac87934a473183aae490b963d4e22ebc282bc365
shektor <s.hektor@gmail.com>
2019-07-05 10:47:31 +0100

    Add priceItems function to Sale class
    Individual items can be priced using the same logic as the total
    function. Iterate through order items, push to an array and return.
    
    Items pushed should contain details including their product_id,
    quantity, price per item, total and vat.

--- spec/unit/sale.spec.js
+++ spec/unit/sale.spec.js
@@ -10,6 +10,44 @@ describe('Sale', () => {
     })
   })
 
+  describe('#priceItems', () => {
+    it('returns an array of priced items based on the order', () => {
+      const order = {
+        items: [
+          {
+            product_id: 1,
+            quantity: 2
+          },
+          {
+            product_id: 4,
+            quantity: 3
+          }
+        ]
+      }
+
+      const sale = new Sale(order, pricing)
+
+      const pricedItems = [
+        {
+          'product_id': 1,
+          'quantity': 2,
+          'price': 599,
+          'total': 1438,
+          'vat': 240
+        },
+        {
+          'product_id': 4,
+          'quantity': 3,
+          'price': 1000,
+          'total': 3000,
+          'vat': 0
+        }
+      ]
+
+      expect(sale.priceItems()).toEqual(pricedItems)
+    })
+  })
+
   describe('#total', () => {
     it('returns order total for items with no VAT', () => {
       const order = {
--- src/models/sale.js
+++ src/models/sale.js
@@ -2,6 +2,30 @@ class Sale {
   constructor (order, prices) {
     this.order = order
     this.prices = prices
+    this.items = []
+  }
+
+  priceItems () {
+    this.order.items.forEach((item) => {
+      const vatBand = this.prices.product[item.product_id].vat_band
+      const vatMultiplier = this.prices.vat_bands[vatBand]
+
+      const itemPrice = this.prices.product[item.product_id].price
+
+      const netItemTotal = itemPrice * item.quantity
+      const vatTotal = this.calculateVAT(vatMultiplier, netItemTotal)
+
+      const itemPriced = {
+        'product_id': item.product_id,
+        'quantity': item.quantity,
+        'price': itemPrice,
+        'total': netItemTotal + vatTotal,
+        'vat': vatTotal
+      }
+
+      this.items.push(itemPriced)
+    })
+    return this.items
   }
 
   total () {
